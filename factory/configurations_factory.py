a2d15f8eb140225497e885d2c3ff0c011a6efdc5
Reagan Mbitiru <reagan@nectar.software>
2021-10-21 17:16:28 +0300

    added get configuration

--- factory/configurations_factory.py
+++ factory/configurations_factory.py
@@ -1,14 +1,14 @@
 from factory.base import Base
 
 
-class Configuration(Base):
+class ConfigurationsFactory(Base):
 
     def __init__(self, key: str, secret: str):
         super().__init__(key, secret)
         self.configuration_path = "/v1/configurations"
 
-    def get_configuration(self, ref: str, detailed: bool):
-        return self.get(self.configuration_path, 'ref={}&detailed={}'.format(ref, detailed), self.content_type)
+    def get_configuration(self, ref: str):
+        return self.get(self.configuration_path, 'ref={}&detailed=false'.format(ref), self.content_type)
 
     def activate_configuration(self, ref: str):
         endpoint = '{}?ref={}'.format(self.configuration_path, ref)
