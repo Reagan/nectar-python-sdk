729448d31b2ac141be0ac7445dbcbefd93422467
Reagan Mbitiru <reagan@nectar.software>
2021-10-22 11:05:08 +0300

    fixed notifications

--- factory/notifications_factory.py
+++ factory/notifications_factory.py
@@ -1,17 +1,19 @@
 from factory.base import Base
 
+from factory.base import create_payload
 
-class Notifications(Base):
+
+class NotificationsFactory(Base):
 
     def __init__(self, key: str, secret: str):
         super().__init__(key, secret)
-        self.notifications_path = "/v1/configurations"
+        self.notifications_path = "/v1/notifications"
 
     def get_notifications(self):
         return self.get(self.notifications_path, '', self.content_type)
 
     def set_notification_read_status(self, notification_ref: str,
-                                     status: bool, timestamp: int):
+                                     status: bool, timestamp: float):
         payload = create_payload({
             'notification_ref': notification_ref,
             'status': status,
